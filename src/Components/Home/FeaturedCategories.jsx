d6d5299846ff4d7aec107c2ca0de9212ef4aaed9
Lee Broadhurst <lee_b250@hotmail.co.uk>
2021-07-16 13:35:34 +0100

    amend FeaturedCategories with dice design

--- src/Components/Home/FeaturedCategories.jsx
+++ src/Components/Home/FeaturedCategories.jsx
@@ -4,12 +4,26 @@ const FeaturedCategories = () => {
   return (
     <div>
       <h1>Featured Categories</h1>
-      <ul className="items-container">
-        <li>Strategy</li>
-        <li>Hidden-Roles</li>
-        <li>Roll-and-Write</li>
-        <li>Dexterity</li>
-        <li>Push-your-Luck</li>
+      <ul className="dice-container">
+        <li>
+          <button className="dice-button">Strategy</button>
+        </li>
+        <li></li>
+        <li>
+          <button className="dice-button">Hidden-Roles</button>
+        </li>
+        <li></li>
+        <li>
+          <button className="dice-button">Roll-and-Write</button>
+        </li>
+        <li></li>
+        <li>
+          <button className="dice-button">Dexterity</button>
+        </li>
+        <li></li>
+        <li>
+          <button className="dice-button">Push-your-Luck</button>
+        </li>
       </ul>
     </div>
   );
